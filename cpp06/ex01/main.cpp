47fb2f17874d871846e242fa5bb6db04d0db32e9
ade-la-c <gringodlc@gmail.com>
2022-02-19 20:39:13 +0100

    tsebien

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,11 +6,18 @@
 /*   By: ade-la-c <ade-la-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 20:26:15 by ade-la-c          #+#    #+#             */
-/*   Updated: 2022/02/19 20:31:02 by ade-la-c         ###   ########.fr       */
+/*   Updated: 2022/02/19 20:37:10 by ade-la-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
+typedef struct s_data {
+
+	std::string		name;
+
+}	Data;
+
 
 uintptr_t	serialize(){}
 
